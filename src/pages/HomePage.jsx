cfcacd00a59081e1cf504f2931f997c736f03a31
federicascognamiglio <scognamigliofede99@gmail.com>
2025-04-09 19:21:09 +0200

    feat: home page content added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,12 @@
+import { Link } from "react-router-dom";
+
 function HomePage() {
     return (
-        <h1>Questa è la Home Page</h1>
+        <div className="container mt-5 d-flex flex-column align-items-center">
+            <h1 className="mt-5 mb-2">BENVENUTI NEL MIO PORTFOLIO</h1>
+            <p className="mb-4">Qui troverete tutti i miei lavori e progetti personali.</p>
+            <Link to="/projects" className="btn btn-outline-primary">Progetti</Link>
+        </div>
     )
 }
 
